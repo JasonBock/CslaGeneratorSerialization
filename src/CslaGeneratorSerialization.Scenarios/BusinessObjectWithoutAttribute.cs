1109a7eabc57ed1f19e406e5368cd447eda21871
Jason Bock <jason.r.bock@outlook.com>
2026-01-11 11:40:41 -0600

    #19 Added Scenarios solution (and that works)

---
+++ src/CslaGeneratorSerialization.Scenarios/BusinessObjectWithoutAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Csla;
+
+namespace CslaGeneratorSerialization.Scenarios;
+
+public partial class Customer
+	: BusinessBase<Customer>
+{
+	[Create]
+	private void Create() { }
+
+	public static readonly PropertyInfo<uint> AgeProperty =
+		Customer.RegisterProperty<uint>(nameof(Customer.Age));
+	public uint Age
+	{
+		get => this.GetProperty(Customer.AgeProperty);
+		set => this.SetProperty(Customer.AgeProperty, value);
+	}
+
+	public static readonly PropertyInfo<string> NameProperty =
+		Customer.RegisterProperty<string>(nameof(Customer.Name));
+	public string Name
+	{
+		get => this.GetProperty(Customer.NameProperty)!;
+		set => this.SetProperty(Customer.NameProperty, value);
+	}
+}
