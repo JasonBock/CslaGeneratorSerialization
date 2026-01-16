8e0c6aa915597d5bba06d74f1d8026e687013ba8
Jason Bock <jason.r.bock@outlook.com>
2026-01-16 08:22:07 -0600

    #19 added more scenarios

---
+++ src/CslaGeneratorSerialization.Scenarios/HasAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Csla;
+
+namespace CslaGeneratorSerialization.Scenarios;
+
+[GeneratorSerializable]
+public partial class HasAttribute
+	: BusinessBase<HasAttribute>
+{
+	[Create]
+	private void Create() { }
+
+	public static readonly PropertyInfo<uint> AgeProperty =
+		HasAttribute.RegisterProperty<uint>(nameof(HasAttribute.Age));
+	public uint Age
+	{
+		get => this.GetProperty(HasAttribute.AgeProperty);
+		set => this.SetProperty(HasAttribute.AgeProperty, value);
+	}
+
+	public static readonly PropertyInfo<string> NameProperty =
+		HasAttribute.RegisterProperty<string>(nameof(HasAttribute.Name));
+	public string Name
+	{
+		get => this.GetProperty(HasAttribute.NameProperty)!;
+		set => this.SetProperty(HasAttribute.NameProperty, value);
+	}
+}
