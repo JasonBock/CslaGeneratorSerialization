7c05ef3d07884c40a90c48cc5653241b06579bdd
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2026-07-28 04:49:55 +0200

    Remove unnecessary suppression

--- src/CslaGeneratorSerialization.Analysis/Polyfilling/CollectionBuilderAttribute.cs
+++ src/CslaGeneratorSerialization.Analysis/Polyfilling/CollectionBuilderAttribute.cs
@@ -2,9 +2,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace System.Runtime.CompilerServices;
-#pragma warning restore IDE0130 // Namespace does not match folder structure
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, Inherited = false)]
 internal sealed class CollectionBuilderAttribute : Attribute
