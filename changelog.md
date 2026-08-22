cb34039a622369321f0c5b2f5c72933fb23ec22b
Jason Bock <jason.r.bock@outlook.com>
2026-08-21 19:56:52 -0500

    #54 all tests pass, time for a merge

--- changelog.md
+++ changelog.md
@@ -5,7 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [3.0.0-alpha.1] - Not Yet Released
+## [3.0.0-alpha.2] - Not Yet Released
+
+### Fixed
+* Recursive unions are now supported (issue [#54](https://github.com/JasonBock/CslaGeneratorSerialization/issues/54))
+
+## [3.0.0-alpha.1] - 2026-08-11
 
 ### Added
 * Added support for union types (issue [#49](https://github.com/JasonBock/CslaGeneratorSerialization/issues/49))
---
+++ src/CslaGeneratorSerialization.IntegrationTests/Graphs/Unions/RecursiveUnionInPropertiesTests.cs
@@ -0,0 +1,46 @@
+﻿using Csla;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace CslaGeneratorSerialization.IntegrationTests.Graphs.Unions.RecursiveUnionInPropertiesTestsDomain;
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+#pragma warning disable IDE0250 // Make struct 'readonly'
+public union DataOne(int, string, DataTwo);
+public union DataTwo(int, string, DataOne);
+
+[GeneratorSerializable]
+public partial class Customer
+	: BusinessBase<Customer>
+{
+	[Create]
+	private void Create() { }
+
+	public static readonly PropertyInfo<DataOne> IdentifierProperty =
+		Customer.RegisterProperty<DataOne>(nameof(Customer.Identifier));
+	public DataOne Identifier
+	{
+		get => this.GetProperty(Customer.IdentifierProperty);
+		set => this.SetProperty(Customer.IdentifierProperty, value);
+	}
+}
+
+internal static class RecursiveUnionInPropertiesTests
+{
+	[Test]
+	public static async Task RoundtripAsync()
+	{
+		var provider = Shared.ServiceProvider;
+		var formatter = new GeneratorFormatter(provider.GetRequiredService<ApplicationContext>(), new(provider));
+		var customerPortal = provider.GetRequiredService<IDataPortal<Customer>>();
+		var customer = await customerPortal.CreateAsync();
+		customer.Identifier = new DataTwo(42);
+
+		using var stream = new MemoryStream();
+		formatter.Serialize(stream, customer);
+		stream.Position = 0;
+		var newCustomer = (Customer)formatter.Deserialize(stream)!;
+
+		Assert.That(((DataTwo)newCustomer.Identifier.Value).Value, Is.EqualTo(42));
+	}
+}
